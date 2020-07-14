d783ff3168c70640b74675c550a816c48d51931c
mitydev <michael.tiago@hotmail.com>
2020-07-13 21:11:54 -0300

    Adicionando horários

--- index.js
+++ index.js
@@ -16,7 +16,24 @@ app.use(bodyParser.json());
 
 app.listen(3000, port);
 mongoose.Promise = global.Promise;
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // ==== CHAMADAS DIRETAS DA BASE DE DADOS DA TR1 ===== //
 app.post('/delivery/auth', async (req, res) => {
     let connect = await mongoose.connect('mongodb://localhost/tr1_delivery', { useUnifiedTopology: true, useNewUrlParser: true });
@@ -32,22 +49,21 @@ app.post('/delivery/auth', async (req, res) => {
         return res.json({err: 'Erro interno do servidor !'});
     }
 });
-app.post('/delivery/client', async function(req, res){
-    await mongoose.connect('mongodb://localhost/tr1_delivery', { useUnifiedTopology: true, useNewUrlParser: true });
-
-
-});
 app.post('/delivery/create', async (req, res) => {
     let connect = await mongoose.connect('mongodb://localhost/tr1_delivery', { useUnifiedTopology: true, useNewUrlParser: true });
     var { database } = req.body;
-    try{
-        await Admin.create(req.body);
-        await mongoose.connect('mongodb://localhost/' + database, { useUnifiedTopology: true, useNewUrlParser: true });
-        await UserAdmin.create(req.body);   
-        return res.status(200).json({success: 'success'});
-    }catch (err){
-        return res.json({err: err});
-    }
+
+   console.log(req)
+
+
+    // try{
+    //     await Admin.create(req.body);
+    //     await mongoose.connect('mongodb://localhost/' + database, { useUnifiedTopology: true, useNewUrlParser: true });
+    //     await UserAdmin.create(req.body);   
+    //     return res.status(200).json({success: 'success'});
+    // }catch (err){
+    //     return res.json({err: err});
+    // }
 });
 // ==== CHAMADAS DIRETAS DA BASE DE DADOS DA TR1 ===== //
 
@@ -62,4 +78,5 @@ app.post('/delivery/request', async function(req, res){
         res.status(500).json({err: 'Erro'});
     }
 }); 
+
 
